db9bb1a3cb5520e5ead13322e79b83c74fdcb912
Your Name <you@example.com>
2024-09-10 04:30:41 +0000

    first commit

---
+++ src/pipeline.py
@@ -0,0 +1,29 @@
+import torch
+from PIL.Image import Image
+from diffusers import StableDiffusionXLPipeline
+from pipelines.models import TextToImageRequest
+from torch import Generator
+
+
+def load_pipeline() -> StableDiffusionXLPipeline:
+    pipeline = StableDiffusionXLPipeline.from_pretrained(
+        "./models/newdream-sdxl-20",
+        torch_dtype=torch.float16,
+        local_files_only=True,
+    ).to("cuda")
+
+    pipeline(prompt="")
+
+    return pipeline
+
+
+def infer(request: TextToImageRequest, pipeline: StableDiffusionXLPipeline) -> Image:
+    generator = Generator(pipeline.device).manual_seed(request.seed) if request.seed else None
+
+    return pipeline(
+        prompt=request.prompt,
+        negative_prompt=request.negative_prompt,
+        width=request.width,
+        height=request.height,
+        generator=generator,
+    ).images[0]
